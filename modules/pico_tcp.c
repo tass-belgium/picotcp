01747c52e345f9b4778671db3f5d9d967beb9691
Daniele <dan@hackbox.(none)>
2012-10-28 19:10:48 +0100

    TCP receiver: SACK implementation.

--- modules/pico_tcp.c
+++ modules/pico_tcp.c
@@ -16,6 +16,12 @@
 #define PICO_TCP_UNREACHABLE    0x04
 
 /* Structure for TCP socket */
+struct tcp_sack_block {
+  uint32_t left;
+  uint32_t right;
+  struct tcp_sack_block *next;
+};
+
 struct pico_socket_tcp {
   struct pico_socket sock;
 
@@ -46,6 +52,7 @@ struct pico_socket_tcp {
   uint8_t ts_ok;
   uint8_t mss_ok;
   uint8_t scale_ok;
+  struct tcp_sack_block *sacks;
 
   /* Transmission */
   uint8_t  x_mode;
@@ -230,7 +237,7 @@ static uint32_t pico_paws(void)
   return long_be(_paws); /*XXX: implement paws */
 }
 
-static void tcp_add_options(struct pico_socket_tcp *ts, struct pico_frame *f, int optsiz)
+static void tcp_add_options(struct pico_socket_tcp *ts, struct pico_frame *f, uint16_t flags, int optsiz)
 {
   uint32_t tsval = long_be(pico_tick);
   uint32_t tsecr = long_be(ts->ts_nxt);
@@ -240,7 +247,7 @@ static void tcp_add_options(struct pico_socket_tcp *ts, struct pico_frame *f, in
 
   memset(f->start, PICO_TCP_OPTION_NOOP, optsiz); /* fill blanks with noop */
 
-  if (optsiz >= 20) { 
+  if (flags & PICO_TCP_SYN) { 
     f->start[i++] = PICO_TCP_OPTION_MSS;
     f->start[i++] = PICO_TCPOPTLEN_MSS;
     f->start[i++] = (ts->mss >> 8) & 0xFF;
@@ -261,14 +268,35 @@ static void tcp_add_options(struct pico_socket_tcp *ts, struct pico_frame *f, in
     memcpy(f->start + i, &tsecr, 4);
     i += 4;
   }
-  f->start[ optsiz - 1 ] = PICO_TCP_OPTION_END;
+
+  if (flags & PICO_TCP_ACK) {
+    struct tcp_sack_block *sb;
+    int len_off;
+
+    if (ts->sack_ok && ts->sacks) {
+      f->start[i++] = PICO_TCP_OPTION_SACK;
+      len_off = i;
+      f->start[i++] = PICO_TCPOPTLEN_SACK;
+      while(ts->sacks) {
+        sb = ts->sacks;
+        ts->sacks = sb->next;
+        memcpy(f->start + i, sb, 2 * sizeof(uint32_t));
+        i += (2 * sizeof(uint32_t));
+        f->start[len_off] += (2 * sizeof(uint32_t));
+        pico_free(sb);
+      }
+    }
+  }
+  if (i < optsiz)
+    f->start[ optsiz - 1 ] = PICO_TCP_OPTION_END;
 }
 
 static void tcp_set_space(struct pico_socket_tcp *t)
 {
-  int mtu = t->mss + PICO_SIZE_TCPHDR + PICO_SIZE_TCPOPT_SYN ;
-  int space;
+  int mtu, space;
   int shift = 0;
+
+  mtu = t->mss + PICO_SIZE_TCPHDR + PICO_SIZE_TCPOPT_SYN ;
   if (t->sock.q_in.max_size == 0) {
     space = 1024 * 1024 * 1024; /* One Gigabyte, for unlimited sockets. */
   } else {
@@ -281,8 +309,10 @@ static void tcp_set_space(struct pico_socket_tcp *t)
     shift++;
   }
   dbg("\n\nSPACE: %d\n\n", space);
-  t->wnd = space;
-  t->wnd_scale = shift;
+  if ((space == 0) || (t->wnd_scale == 0) || (shift != t->wnd_scale) || ((space - t->wnd) > (space>>2))) {
+    t->wnd = space;
+    t->wnd_scale = shift;
+  }
 }
 
 
@@ -290,6 +320,7 @@ static void tcp_set_space(struct pico_socket_tcp *t)
 static int tcp_options_size(struct pico_socket_tcp *t, uint16_t flags)
 {
   int size = 0;
+  struct tcp_sack_block *sb = t->sacks;
 
   if (flags & PICO_TCP_SYN) {  /* Full options */ 
     size = PICO_TCPOPTLEN_MSS + PICO_TCP_OPTION_SACK_OK + PICO_TCPOPTLEN_WS + PICO_TCPOPTLEN_TIMESTAMP;
@@ -302,6 +333,14 @@ static int tcp_options_size(struct pico_socket_tcp *t, uint16_t flags)
       size += PICO_TCPOPTLEN_TIMESTAMP;
 
     size+= PICO_TCPOPTLEN_END;
+  }
+  if ((flags & PICO_TCP_ACK) && (t->sack_ok && sb)) {
+    size += 2;
+    while(sb) {
+      size += (2 * sizeof(uint32_t));
+      dbg("Adding SACK information.\n");
+      sb = sb->next;
+    }
   }
   size = (((size + 3) >> 2) << 2);
   return size;
@@ -509,7 +548,7 @@ int pico_tcp_initconn(struct pico_socket *s)
   hdr->rwnd = short_be(ts->wnd);
   ts->snd_una = short_be(hdr->seq);
   tcp_set_space(ts);
-  tcp_add_options(ts,syn, opt_len);
+  tcp_add_options(ts,syn, PICO_TCP_SYN, opt_len);
   tcp_send(ts, syn);
   return 0;
 }
@@ -536,14 +575,13 @@ static int tcp_send_synack(struct pico_socket *s)
   ts->rcv_processed = long_be(hdr->seq);
   ts->snd_last = ts->snd_nxt + 1;
   tcp_set_space(ts);
-  tcp_add_options(ts,synack, opt_len);
+  tcp_add_options(ts,synack, hdr->flags, opt_len);
   synack->payload_len = 0;
   synack->timestamp = pico_tick;
   //pico_enqueue_segment(&s->q_out,synack);
   tcp_send(ts, synack);
   return 0;
 }
-
 
 
 static void tcp_send_ack(struct pico_socket_tcp *t)
@@ -562,9 +600,62 @@ static void tcp_send_ack(struct pico_socket_tcp *t)
   hdr->flags = PICO_TCP_ACK;
   hdr->rwnd = short_be(t->wnd);
   tcp_set_space(t);
-  tcp_add_options(t,f, opt_len);
+  tcp_add_options(t,f, PICO_TCP_ACK, opt_len);
   tcp_send(t,f);
 
+}
+
+static void tcp_sack_prepare(struct pico_socket_tcp *t)
+{
+  struct pico_frame *pkt;
+  uint32_t left=0, right=0;
+  struct tcp_sack_block *sb;
+  if (t->sacks) /* previous sacks are pending */
+    return;
+
+  pkt = pico_queue_peek_first(&t->sock.q_in);
+  while(1) {
+    dbg("Packet is %08x, rcv_nxt is %08x\n", pkt?SEQN(pkt):0, t->rcv_nxt);
+    if (!pkt) {
+      if(left) {
+        sb = pico_zalloc(sizeof(struct tcp_sack_block));
+        sb->left = long_be(left);
+        sb->right = long_be(right);
+        sb->next = t->sacks;
+        t->sacks = sb;
+        left = 0;
+        right = 0;
+      }
+      break;
+    }
+    if ((SEQN(pkt) < t->rcv_nxt)) {
+      dbg("sack: skipping pkt\n");
+      pkt = pkt->next;
+      continue;
+    }
+    if (!left) {
+      left = SEQN(pkt);
+      right = SEQN(pkt) + pkt->payload_len;
+      pkt = pkt->next;
+      continue;
+    }
+    if(SEQN(pkt) == (right + 1)) {
+      right += pkt->payload_len;
+      pkt = pkt->next;
+      continue;
+    } else {
+      sb = pico_zalloc(sizeof(struct tcp_sack_block));
+      if (!sb)
+        break;
+      sb->left = long_be(left);
+      sb->right = long_be(right);
+      sb->next = t->sacks;
+      t->sacks = sb;
+      left = 0;
+      right = 0;
+      pkt = pkt->next;
+    }
+  }
 }
 
 static int tcp_data_in(struct pico_socket *s, struct pico_frame *f)
@@ -582,12 +673,15 @@ static int tcp_data_in(struct pico_socket *s, struct pico_frame *f)
       pico_enqueue_segment(&t->sock.q_in, f);
       if (seq_compare(SEQN(f), t->rcv_nxt) == 0) {
         dbg("TCP> exactly what I was expecting!\n");
-        if (t->sock.wakeup)
+        if (t->sock.wakeup) {
           t->sock.wakeup(PICO_SOCK_EV_RD, &t->sock);
-          t->rcv_nxt = SEQN(f) + f->payload_len;
+        }
+        t->rcv_nxt = SEQN(f) + f->payload_len;
       } else {
         dbg("TCP> hi segment. Possible packet loss. I'll dupack this. (exp: %x got: %x)\n", t->rcv_nxt, SEQN(f));
-        /* XXX: differentiate dupack to insert sacks. */
+        if (t->sack_ok) {
+          tcp_sack_prepare(t);
+        }
       }
 
     }
@@ -919,12 +1013,14 @@ void pico_tcp_output(struct pico_socket *s)
 {
   struct pico_socket_tcp *t = (struct pico_socket_tcp *)s;
   struct pico_frame *f;
+  struct pico_tcp_hdr *hdr; 
 
   f = pico_queue_peek(&s->q_out, t->snd_nxt + 1);
   while(f && (t->in_flight <= t->cwnd)) {
+    hdr = (struct pico_tcp_hdr *)f->transport_hdr;
     dbg("TCP> DEQUEUED (for output) frame %08x, len= %d\n", SEQN(f), f->payload_len);
     f->timestamp = pico_tick;
-    tcp_add_options(t, f, tcp_options_size(t, f->flags));
+    tcp_add_options(t, f, hdr->flags, tcp_options_size(t, hdr->flags));
     tcp_send(t, pico_frame_copy(f));
     t->in_flight++;
     f = pico_queue_peek(&s->q_out, t->snd_nxt + 1);
