9e77d6440747063551efa5fd51f5fc6b94ceccda
Daniele Lacamera <root@danielinux.net>
2013-02-16 18:48:17 +0100

    Changed LICENSE to GPL2. Removed the do-not-distribute clause from the source headers

--- include/heap.h
+++ include/heap.h
@@ -1,8 +1,6 @@
 /*********************************************************************
 PicoTCP. Copyright (c) 2012 TASS Belgium NV. Some rights reserved.
 See LICENSE and COPYING for usage.
-Do not redistribute without a written permission by the Copyright
-holders.
 
 *********************************************************************/
 #include <stdint.h>
--- modules/pico_dhcp_common.h
+++ modules/pico_dhcp_common.h
@@ -1,8 +1,8 @@
 /*********************************************************************
 PicoTCP. Copyright (c) 2012 TASS Belgium NV. Some rights reserved.
 See LICENSE and COPYING for usage.
-Do not redistribute without a written permission by the Copyright
-holders.
+
+.
 
 *********************************************************************/
 #ifndef _INCLUDE_PICO_DHCP_COMMON
