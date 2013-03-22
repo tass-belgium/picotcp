5b1fe7dccb2d1f6310d5bb3e5491929f3f7d94a0
Kristof Roelants <kristof.roelants@tass.be>
2013-03-22 12:53:41 +0100

    Task #572 Added TCP CRC recalculation to reassembled frame
    This recalculation is optional, but added for consistency.
    The TCP CRC of the first fragmented packet we receive is calculated
    over the inital unfragmented packet. When the packet is reassembled
    the data over which the CRC is calculated is equal to the data of
    the initial packet.

--- modules/pico_ipv4.c
+++ modules/pico_ipv4.c
@@ -238,7 +238,8 @@ static inline int pico_ipv4_fragmented_check(struct pico_protocol *self, struct 
   uint16_t offset = 0;
   uint16_t data_len = 0;
   struct pico_ipv4_hdr *f_frag_hdr = NULL, *hdr = (struct pico_ipv4_hdr *) (*f)->net_hdr;
-  struct pico_udp_hdr *udp_hdr = (struct pico_udp_hdr *) (*f)->transport_hdr;
+  struct pico_udp_hdr *udp_hdr = NULL;
+  struct pico_tcp_hdr *tcp_hdr = NULL;
   struct pico_ipv4_fragmented_packet *pfrag = NULL, frag; 
   struct pico_frame *f_new = NULL, *f_frag = NULL;
   struct pico_tree_node *index, *_tmp;
@@ -354,7 +355,9 @@ static inline int pico_ipv4_fragmented_check(struct pico_protocol *self, struct 
       hdr->crc = short_be(pico_checksum(hdr, PICO_SIZE_IP4HDR));
       /* Optional, the UDP/TCP CRC should already be correct */
       if (hdr->proto == PICO_PROTO_TCP) {
-        //pico_tcp_checksum_ipv4(f);
+        tcp_hdr = (struct pico_tcp_hdr *) f_new->transport_hdr;
+        tcp_hdr->crc = 0;
+        tcp_hdr->crc = short_be(pico_tcp_checksum_ipv4(f_new));
       } else if (hdr->proto == PICO_PROTO_UDP){
         udp_hdr = (struct pico_udp_hdr *) f_new->transport_hdr;
         udp_hdr->crc = 0;
