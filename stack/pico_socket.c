7ef424f158ca0cd395edf832e1a159a0badad0cf
Daniele Lacamera <root@danielinux.net>
2013-03-28 16:34:54 +0100

    Fixed return value of recv/recvfrom in UDP, when no data available

--- stack/pico_socket.c
+++ stack/pico_socket.c
@@ -724,7 +724,8 @@ int pico_socket_recvfrom(struct pico_socket *s, void *buf, int len, void *orig, 
   }
 #ifdef PICO_SUPPORT_UDP 
   if (PROTO(s) == PICO_PROTO_UDP) {
-    return pico_udp_recv(s, buf, len, orig, remote_port);
+    if (pico_udp_recv(s, buf, len, orig, remote_port) < 0)
+      return 0;
   }
 #endif
 #ifdef PICO_SUPPORT_TCP
