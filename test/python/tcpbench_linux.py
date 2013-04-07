50cb16498688b0418ef29fb57b41dfa7f9ad9259
Daniele Lacamera <root@danielinux.net>
2013-04-07 18:59:47 +0200

    Added delay to linux tests

--- test/python/tcpbench_linux.py
+++ test/python/tcpbench_linux.py
@@ -5,8 +5,8 @@ T = Topology()
 net1 = Network(T, "pyt0")
 
 #h1 = Host(T, net1)
-h2 = Host(T, net1, args="tcpbench:r:")
-h3 = Host(T, net1, args="tcpbench:t:172.16.1.1:")
+h2 = Host(T, net1, args="tcpbench:r:", delay1="10")
+h3 = Host(T, net1, args="tcpbench:t:172.16.1.1:", delay1="10")
 
 sleep(1)
 start(T)
