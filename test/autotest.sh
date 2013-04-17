86d6f8a74972739309cd8bfcb3a4da45927f68b1
Andrei Carp <andrei@andrei-Vostro-3560.(none)>
2013-04-17 14:56:15 +0300

    added tcpbench to tcp nat test

--- test/autotest.sh
+++ test/autotest.sh
@@ -12,32 +12,28 @@ killall picoapp.elf
 echo "TCP TEST"
 (./build/test/picoapp.elf --vde pic0:/tmp/pic0.ctl:10.40.0.8:255.255.0.0: -a tcpbench:r:6667:) &
 time (./build/test/picoapp.elf --vde pic0:/tmp/pic0.ctl:10.40.0.9:255.255.0.0: -a tcpbench:t:10.40.0.8:6667: || exit 1)
-wait || exit 1
-wait
+killall picoapp.elf
 
 echo "UDP TEST"
-(./build/test/picoapp.elf --vde pic0:/tmp/pic0.ctl:10.40.0.8:255.255.0.0: -a udpecho:10.40.0.8:6667 >/dev/null) &
-./build/test/picoapp.elf --vde pic0:/tmp/pic0.ctl:10.40.0.9:255.255.0.0: -a udpclient:10.40.0.8:6667:1400:100:10 >/dev/null || exit 1
+(./build/test/picoapp.elf --vde pic0:/tmp/pic0.ctl:10.40.0.8:255.255.0.0: -a udpecho:10.40.0.8:6667:) &
+./build/test/picoapp.elf --vde pic0:/tmp/pic0.ctl:10.40.0.9:255.255.0.0: -a udpclient:10.40.0.8:6667:1400:100:10: || exit 1
 wait || exit 1
 wait
 
 echo "NAT TCP TEST"
 (./build/test/picoapp.elf --vde pic0:/tmp/pic0.ctl:10.40.0.10:255.255.0.0: --vde pic1:/tmp/pic1.ctl:10.50.0.10:255.255.0.0: -a natbox:10.50.0.10) &
 sleep 2
-(./build/test/picoapp.elf --vde pic0:/tmp/pic1.ctl:10.50.0.8:255.255.0.0: -a tcpecho:6667 >/dev/null) &
+(./build/test/picoapp.elf --vde pic0:/tmp/pic1.ctl:10.50.0.8:255.255.0.0: -a tcpbench:r:6667) &
 sleep 2
-./build/test/picoapp.elf --vde pic0:/tmp/pic0.ctl:10.40.0.9:255.255.0.0:10.40.0.10: -a tcpclient:10.50.0.8:6667: || exit 1
-
-echo "Waiting for echo server to finish..."
-wait %2 || exit 1
-
+./build/test/picoapp.elf --vde pic0:/tmp/pic0.ctl:10.40.0.9:255.255.0.0:10.40.0.10: -a tcpbench:t:10.50.0.8:6667: || exit 1
 killall picoapp.elf
 
 echo "NAT UDP TEST"
 (./build/test/picoapp.elf --vde pic0:/tmp/pic0.ctl:10.40.0.10:255.255.0.0: --vde pic1:/tmp/pic1.ctl:10.50.0.10:255.255.0.0: -a natbox:10.50.0.10) &
-(./build/test/picoapp.elf --vde pic0:/tmp/pic1.ctl:10.50.0.8:255.255.0.0: -a udpecho:10.50.0.8:6667 >/dev/null) &
+(./build/test/picoapp.elf --vde pic0:/tmp/pic1.ctl:10.50.0.8:255.255.0.0: -a udpecho:10.50.0.8:6667:) &
 ./build/test/picoapp.elf --vde pic0:/tmp/pic0.ctl:10.40.0.9:255.255.0.0:10.40.0.10: -a udpclient:10.50.0.8:6667:1400:100:10 || exit 1
-wait %2 || exit 1
+#sometimes udpecho finishes before reaching wait %2
+wait %2
 
 killall picoapp.elf
 
@@ -52,13 +48,13 @@ sleep 2
 killall picoapp.elf
 
 echo "DHCP TEST"
-(./build/test/picoapp.elf --vde pic0:/tmp/pic0.ctl:10.40.0.1:255.255.0.0: -a dhcpserver:pic0:10.40.0.1:255.255.255.0) &
+(./build/test/picoapp.elf --vde pic0:/tmp/pic0.ctl:10.40.0.1:255.255.0.0: -a dhcpserver:pic0:10.40.0.1:255.255.255.0:10.40.0.40:10.40.0.140:) &
 ./build/test/picoapp.elf --barevde pic0:/tmp/pic0.ctl: -a dhcpclient:pic0 || exit 1
 killall picoapp.elf
 
 echo "DHCP DUAL TEST"
-(./build/test/picoapp.elf --vde pic0:/tmp/pic0.ctl:10.40.0.1:255.255.0.0: -a dhcpserver:pic0:10.40.0.1:255.255.255.0) &
-(./build/test/picoapp.elf --vde pic1:/tmp/pic1.ctl:10.50.0.1:255.255.0.0: -a dhcpserver:pic1:10.50.0.1:255.255.255.0) &
+(./build/test/picoapp.elf --vde pic0:/tmp/pic0.ctl:10.40.0.1:255.255.0.0: -a dhcpserver:pic0:10.40.0.1:255.255.255.0:10.40.0.40:10.40.0.140:) &
+(./build/test/picoapp.elf --vde pic1:/tmp/pic1.ctl:10.50.0.1:255.255.0.0: -a dhcpserver:pic1:10.50.0.1:255.255.255.0:10.50.0.50:10.50.0.150:) &
 ./build/test/picoapp.elf --barevde pic0:/tmp/pic0.ctl: --barevde pic1:/tmp/pic1.ctl: -a dhcpdualclient:pic0:pic1 || exit 1
 killall picoapp.elf
 
