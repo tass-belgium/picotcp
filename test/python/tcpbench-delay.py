23e3f80a209c5faf4791ba99c6d1962ff2c2cc15
Daniele Lacamera <root@danielinux.net>
2013-07-02 21:30:40 +0200

    Added tcpbench-delay test.
    Restore automatic benchmark on Jenkins

--- test/python/tcpbench-delay.py
+++ test/python/tcpbench-delay.py
@@ -5,8 +5,9 @@ from  topology import *
 
 T = Topology()
 net1 = Network(T)
+
 send1 = Host(T, net1, args="tcpbench:t:172.16.1.2:7770:")
-recv1 = Host(T, net1, args="tcpbench:r:7770:")
+recv1 = Host(T, net1, args="tcpbench:r:7770:", delay1="20", loss1="5")
 
 
 sleep(1)
