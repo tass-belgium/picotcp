fa2673dc2e065dc9a3ee0f1e9efa7a3556945062
Daniele Lacamera <daniele.lacamera@tass.be>
2013-09-21 14:11:41 +0200

    Removed attribute "unused" from zmq_send in the API header

--- modules/pico_zmq.h
+++ modules/pico_zmq.h
@@ -7,7 +7,7 @@ typedef struct zmq_socket *ZMQ;
 ZMQ zmq_publisher(uint16_t _port, void (*cb)(ZMQ z));
 ZMQ zmq_subscriber(void (*cb)(ZMQ z));
 int zmq_connect(ZMQ z, char *address, uint16_t port);
-int __attribute__((unused)) zmq_send(ZMQ z, char *txt, int len);
+int zmq_send(ZMQ z, char *txt, int len);
 int zmq_recv(ZMQ z, char *txt);
 void zmq_close(ZMQ z);
 
