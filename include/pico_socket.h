df49dbc97143032b664a80b1e3ea3ebafeff093d
iulian <iulian@iulian-VirtualBox.(none)>
2013-10-16 14:43:20 +0300

    Issue 36 update

--- include/pico_socket.h
+++ include/pico_socket.h
@@ -133,8 +133,8 @@ struct pico_ip_mreq_source {
 # define PICO_IP_DEFAULT_MULTICAST_TTL        1
 # define PICO_IP_DEFAULT_MULTICAST_LOOP       1
 
-#define PICO_SOCKET_TIMEOUT					  90000u //1.5 minutes
-#define PICO_SOCKET_BOUND_TIMEOUT             15000u //15 sec
+#define PICO_SOCKET_TIMEOUT					  90000u //90 seconds
+#define PICO_SOCKET_BOUND_TIMEOUT             30000u //30 seconds
 
 #define PICO_SOCKET_SHUTDOWN_WRITE 0x01
 #define PICO_SOCKET_SHUTDOWN_READ  0x02
