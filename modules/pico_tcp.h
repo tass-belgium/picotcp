b6002bc6f541bbd0b6de62781d7ba9324e667265
iulian <iulian@iulian-VirtualBox.(none)>
2013-12-10 14:51:23 +0200

    Issue #76 fixed. update

--- modules/pico_tcp.h
+++ modules/pico_tcp.h
@@ -12,7 +12,6 @@
 #include "pico_socket.h"
 
 extern struct pico_protocol pico_proto_tcp;
-extern void transport_flags_update(struct pico_frame *, struct pico_socket *);
 
 struct __attribute__((packed)) pico_tcp_hdr {
     struct pico_trans trans;
@@ -97,5 +96,6 @@ int pico_tcp_queue_in_is_empty(struct pico_socket *s);
 int pico_tcp_reply_rst(struct pico_frame *f);
 void pico_tcp_cleanup_queues(struct pico_socket *sck);
 void pico_tcp_notify_closing(struct pico_socket *sck);
+void transport_flags_update(struct pico_frame *, struct pico_socket *);
 
 #endif
