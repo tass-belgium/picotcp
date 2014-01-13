bf4b33743be56e0697cb721c8b6fad3885a3a609
Alexander Van Parys <alexander.van.parys@tass.be>
2014-01-13 14:11:14 +0100

    Updated timers unittest
    When inserting a timer with a lower expire time than an already
    existing timer, the sequence of the timers is rearranged.
    
    With the timer for ARP flooding protection being already present, and
    having a higher expire time than the timers that were added in the test,
    the order in which the timers are stored was not linear anymore. This
    caused the unittest to fail as it expects the timers to be stored
    in the same order they were inserted.
    
    The expire time for the unittest timers is set to a very large
    number to avoid this problem.

--- test/units.c
+++ test/units.c
@@ -2421,17 +2421,16 @@ END_TEST
 
 START_TEST (test_timers)
 {
-
     struct pico_timer *T[128];
     int i;
     pico_stack_init();
     for (i = 0; i < 128; i++) {
-        T[i] = pico_timer_add(1 + i, 0xff00 + i, 0xaa00 + i);
+        T[i] = pico_timer_add(999999 + i, 0xff00 + i, 0xaa00 + i);
         printf("New timer @ %p (%x-%x)\n", T[i], T[i]->timer, T[i]->arg);
     }
     for (i = 0; i < 128; i++) {
         fail_if(i + 1 > Timers->n);
-        fail_unless(Timers->top[i + 1].tmr == T[i]);
+        fail_unless(Timers->top[i + 2].tmr == T[i]);
         fail_unless(T[i]->timer == (0xff00 + i));
         fail_unless(T[i]->arg == (0xaa00 + i));
     }
@@ -2439,7 +2438,7 @@ START_TEST (test_timers)
         printf("Deleting timer %d \n", i );
         pico_timer_cancel(T[i]);
         printf("Deleted timer %d \n", i );
-        fail_unless(Timers->top[i + 1].tmr == NULL);
+        fail_unless(Timers->top[i + 2].tmr == NULL);
     }
     pico_stack_tick();
     pico_stack_tick();
