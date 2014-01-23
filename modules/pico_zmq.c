cc6081c29253f48b6ad5234bfa34473c43cb6adf
phalox <info@phalox.be>
2014-01-23 11:33:32 +0100

    Improved implementation of ZMQ and ZMTP
    * Parses the header
    * Works for both publisher and subscriber
    
    Tested:
    * Subscriber (on PIC24F) to python publisher was working
    * Publischer (on PIC24F) to python subscriber crashed on the PIC
    
    * Don't trust any code in this file. It's still under heavy development
    * and cleanup

--- modules/pico_zmq.c
+++ modules/pico_zmq.c
@@ -13,6 +13,12 @@
 
 #define MY_VERSION 1u
 
+#define ZMQ_FLAG_MORE_FRAMES 0x01
+#define ZMQ_FLAG_LONG_MESSAGE 0x02
+
+#define ZMQ_FRAME_SHORT_LEN 1
+#define ZMQ_FRAME_LONG_LEN 8
+
 
 enum zmq_state {
     ST_OPEN = 0,
@@ -24,16 +30,23 @@ enum zmq_state {
     ST_BUSY
 };
 
+enum zmq_read_state {
+    ST_RD_FLAG = 0,
+    ST_RD_LEN,
+    ST_RD_BUF,
+    ST_RD_RDY
+};
+
 enum zmq_role {
     ROLE_NONE = 0,
     ROLE_PUBLISHER,
     ROLE_SUBSCRIBER
 };
 
-struct __attribute__((packed)) zmq_msg {
+struct zmq_msg {
     uint8_t flags;
-    uint8_t len;
-    char txt[];
+    uint64_t len;
+    uint8_t *buf;
 };
 
 struct zmq_socket;
@@ -43,8 +56,11 @@ struct zmq_connector {
     enum zmq_state state;
     ZMQ parent;
     enum zmq_role role;
-    uint8_t bytes_received;
+    uint8_t signature[20];
+    uint16_t bytes_received;
     struct zmq_connector *next;
+    enum zmq_read_state read_state;
+    struct zmq_msg msg_read;
 };
 
 struct zmq_socket {
@@ -54,6 +70,24 @@ struct zmq_socket {
     struct zmq_connector *subs;
     enum zmq_role role;
 };
+
+static void print_role(enum zmq_role r)
+{
+    switch(r)
+    {
+        case ROLE_NONE:
+            dbg("Role is: No Role\n");
+            break;
+
+        case ROLE_PUBLISHER:
+            dbg("Role is: Publisher\n");
+            break;
+
+        case ROLE_SUBSCRIBER:
+            dbg("Role is: Subscriber\n");
+            break;
+    }
+}
 
 static int zmq_socket_cmp(void *ka, void *kb)
 {
@@ -155,45 +189,85 @@ static void zmq_check_state(ZMQ z)
 }
 
 
-static void zmq_hs_connected(struct zmq_connector *z)
+static void zmq_hs_connected(struct zmq_connector *zc)
 {
+    ZMQ z = zc->parent;
     /* v2 signature */
-    uint8_t my_signature[14] =  {
-        0xff, 0, 0, 0, 0, 0, 0, 0, 1, 0x7f, 1, 1, 0, 0
+    uint8_t my_signature[17] =  {
+        0xff, 0, 0, 0, 0, 0, 0, 0, 1, 0x7f, /* The actual V2 signature */
+        1,                                  /* Protocol version V2.0 */
+        0,                                  /* Socket type */
+        0, 0,                               /* Identity fields, not used */
+        0, 1, 1                             /* Subs only: SUBSCRIBE msg */
     };
+    dbg("Connected - Sending signature\n");
+    print_role(z->role);
+    /* Set the actual socket type */
+    my_signature[11] = z->role;
 
-/*  uint8_t my_ver[2] = {MY_VERSION, 0}; */
-/*  uint8_t my_greeting[52] = {'N','U','L','L', 0}; */
+    if(z->role == ROLE_SUBSCRIBER)
+        pico_socket_write(zc->sock, my_signature, 17);
+    else
+        pico_socket_write(zc->sock, my_signature, 14);
 
-    pico_socket_write(z->sock, my_signature, 14);
-/*  pico_socket_write(z->sock, my_ver, 2); */
-
-/*  if (MY_VERSION > 2) */
-/*    pico_socket_write(z->sock, my_greeting, 52); */
-
-    z->state = ST_SIGNATURE;
-/*  z->state = ST_RDY; */
+    zc->bytes_received = 0;
+    zc->read_state = ST_RD_FLAG;
+    zc->state = ST_SIGNATURE;
 }
 
 static void zmq_hs_signature(struct zmq_connector *zc)
 {
-    uint8_t incoming[20];
     int ret;
+    int req_size = 0;
+    ZMQ z = zc->parent;
+    dbg("Signature handler called\n");
+    if(z->role == ROLE_SUBSCRIBER)
+        req_size = 14;
+    else
+        req_size = 17;
 
-    ret = pico_socket_read(zc->sock, incoming, 14);
-    if (zc->bytes_received == 0 && ret > 0 &&  incoming[0] != 0xFF) {
-        /* dbg("Received invalid signature: [0]!=0xFF\n"); */
+    dbg("Going to read from socket %d bytes\n", req_size);
+    ret = pico_socket_read(zc->sock, (zc->signature + zc->bytes_received), (req_size - zc->bytes_received));
+    dbg("Read %d bytes from socket\n", ret);
+    if (ret < 0) {
+        dbg("Socket error\n");
         zmq_connector_del(zc);
-    }
-
-    zc->bytes_received = (uint8_t)(zc->bytes_received + ret);
-    if (zc->bytes_received < 14) {
-        /* dbg("Waiting for the rest of the sig - got %u bytes\n",zc->bytes_received); */
         return;
     }
 
-    /* dbg("Valid signature received. len = %d, first byte: %02x\n", ret, incoming[0]); */
-    zc->state = ST_RDY;
+    zc->bytes_received += ret;
+
+    if(zc->bytes_received == req_size)
+    {
+        if (zc->signature[0] != 0xFF || zc->signature[9] != 0x7F)
+        {
+            dbg("Received invalid signature - closing connection\n");
+            zmq_connector_del(zc);
+            return;
+        }
+
+        /* If we're a publisher we need to double check */
+        if(z->role == ROLE_PUBLISHER)
+        {
+            if(zc->signature[11] != ROLE_SUBSCRIBER)
+            {
+                dbg("A non subscriber is trying to connect to us publishers\n");
+                zmq_connector_del(zc);
+                return;
+            }
+            if(zc->signature[16] != 0x01)
+            {
+                dbg("This subscriber is not interested in subscribing\n");
+                zmq_connector_del(zc);
+                return;
+            }
+        }
+
+        dbg("State is ready\n");
+        zc->bytes_received = 0;
+        zc->state = ST_RDY;
+    }
+    return;
 }
 
 static void zmq_hs_version(struct zmq_connector *zc)
@@ -248,15 +322,113 @@ static void zmq_hs_greeting(struct zmq_connector *zc)
 
 static void zmq_hs_rdy(struct zmq_connector *zc)
 {
-    uint8_t incoming[258];
     int ret;
-    (void)ret;
-
+    ZMQ z = zc->parent;
+    uint8_t incoming[258];
     if (zc->role == ROLE_SUBSCRIBER)
+    {
+        if(ST_RD_FLAG == zc->read_state)
+        {
+            ret = pico_socket_read(zc->sock, &zc->msg_read.flags, 1);
+            if(ret < 0)
+            {
+                goto sock_err;
+            }
+            else if (1 == ret)
+            {
+                zc->read_state = ST_RD_LEN;
+                zc->bytes_received = 0;
+                dbg("Flags read %x\n", zc->msg_read.flags);
+            }
+        }
+
+        if(ST_RD_LEN == zc->read_state)
+        {
+            if(zc->msg_read.flags & ZMQ_FLAG_LONG_MESSAGE)
+            {
+                ret = pico_socket_read(zc->sock, (&zc->msg_read.len + zc->bytes_received), (ZMQ_FRAME_LONG_LEN - zc->bytes_received));
+                if(ret < 0)
+                {
+                    goto sock_err;
+                }
+                zc->bytes_received += ret;
+
+                dbg("Reading length, bytes received %d\n", ret);
+
+                /* Full 8 bytes length received */
+                if(ZMQ_FRAME_LONG_LEN == zc->bytes_received)
+                {
+                    /* Go from big endian to system endian*/
+                    zc->msg_read.len = long_long_be(zc->msg_read.len);
+
+                    if(zc->msg_read.len >= 1L<<16)
+                    {
+                        dbg("We cannot handle more than 65 536 bytes length\n");
+                    }
+                    dbg("Length2 is %llu\n", zc->msg_read.len);
+                    zc->read_state = ST_RD_BUF;
+                    zc->bytes_received = 0;
+                    zc->msg_read.buf = pico_zalloc((uint16_t)zc->msg_read.len);
+                    if(!zc->msg_read.buf)
+                    {
+                        dbg("No more memory :(\n");
+                        return;
+                    }
+                }
+            }
+            else
+            {
+                uint8_t short_len;
+                ret = pico_socket_read(zc->sock, &short_len, 1);
+                if(ret < 0)
+                {
+                    goto sock_err;
+                }
+                else if (1 == ret)
+                {
+                    dbg("Short length is %u\n", short_len);
+                    zc->msg_read.len = (uint64_t)short_len;
+                    dbg("Length is %llu\n", zc->msg_read.len);
+                    zc->read_state = ST_RD_BUF;
+                    zc->bytes_received = 0;
+                    zc->msg_read.buf = pico_zalloc((uint16_t)zc->msg_read.len);
+                    if(!zc->msg_read.buf)
+                    {
+                        dbg("No more memory :(\n");
+                        return;
+                    }
+                }
+            }
+        }
+
+        if(ST_RD_BUF == zc->read_state)
+        {
+            ret = pico_socket_read(zc->sock, (zc->msg_read.buf + zc->bytes_received), (zc->msg_read.len - zc->bytes_received));
+            if(ret < 0)
+            {
+                goto sock_err;
+            }
+            zc->bytes_received += ret;
+
+            /* Full length received */
+            if(zc->msg_read.len == zc->bytes_received)
+            {
+                zc->read_state = ST_RD_RDY;
+                /* Callback */
+                dbg("Calling ZMQ cb\n");
+                z->ready(z);
+            }
+        }
         return;
+    }
 
     ret = pico_socket_read(zc->sock, incoming, 258);
     dbg("Got %d bytes from subscriber whilst in rdy state.\n", ret);
+
+sock_err:
+    dbg("Error reading!\n");
+    zmq_connector_del(zc);
+    return;
 }
 
 static void zmq_hs_busy(struct zmq_connector *zc)
@@ -302,7 +474,9 @@ static void cb_tcp0mq(uint16_t ev, struct pico_socket *s)
             if (z->state == ST_OPEN) {
                 dbg("tcp0mq> Accepted connection! New subscriber on sock %p.\n", z_a->sock);
                 zmq_connector_add(z, z_a);
-                z_a->role = ROLE_PUBLISHER;
+                z_a->role = ROLE_SUBSCRIBER;
+                dbg("Received new connection\n");
+                print_role(z_a->role);
                 z_a->state = ST_CONNECTED;
                 zmq_hs_connected(z_a);
             } else {
@@ -323,17 +497,21 @@ static void cb_tcp0mq(uint16_t ev, struct pico_socket *s)
 
     if ((ev & PICO_SOCK_EV_CONN) && zc->role == ROLE_SUBSCRIBER && zc->state == ST_OPEN)
     {
+        dbg("Socket %p is set to connected\n", s);
         zc->state = ST_CONNECTED;
         zmq_hs_connected(zc);
     }
 
 
     if (ev & PICO_SOCK_EV_RD) {
+        dbg("Socket %p Can be read \n", s);
         if (zmq_hs_cb[zc->state])
             zmq_hs_cb[zc->state](zc);
     }
 
+
     if ((ev & PICO_SOCK_EV_WR) && zc->parent && (zc->parent->role == ROLE_PUBLISHER) && (zc->state == ST_BUSY)) {
+        dbg("Socket %p Can be written to \n", s);
         if (zmq_hs_cb[zc->state])
             zmq_hs_cb[zc->state](zc);
     }
@@ -399,6 +577,7 @@ int zmq_connect(ZMQ z, char *address, uint16_t port)
     if (pico_socket_connect(z_c->sock, &ip, short_be(port)) < 0)
         return -1;
 
+    z->state = ST_RDY;
     zmq_connector_add(z, z_c);
     return 0;
 }
@@ -450,8 +629,11 @@ ZMQ zmq_publisher(uint16_t _port, void (*cb)(ZMQ z))
 
 int zmq_send(ZMQ z, char *txt, int len)
 {
-    struct zmq_msg *msg;
+    uint8_t *buffer;
     struct zmq_connector *c = z->subs;
+    uint16_t len_int = (uint16_t) len;
+    uint64_t _len = (uint64_t)len_int, _len_be;
+    uint16_t total_size;
     int ret = 0;
 
     if (!c)
@@ -459,43 +641,61 @@ int zmq_send(ZMQ z, char *txt, int len)
         dbg("no subscribers, bailing out\n");
         return 0; /* Need at least one subscriber */
     }
+    if(len_int > 255)
+    {
+        total_size = len_int + ZMQ_FRAME_LONG_LEN + 1;
+        buffer = pico_zalloc((size_t)(total_size));
+        buffer[0] = 0x02;
+        _len_be = long_long_be(_len);
+        memcpy(&buffer[1], &_len_be, sizeof(_len_be));
+        memcpy(&buffer[9], txt, len_int);
 
-    msg = pico_zalloc((size_t)(len + 2));
-    msg->flags = 4;
-    msg->len = (uint8_t) len;
-    memcpy(msg->txt, txt, (size_t) len);
+    }
+    else
+    {
+        total_size = len_int + ZMQ_FRAME_SHORT_LEN + 1;
+        buffer = pico_zalloc((size_t)(len_int + ZMQ_FRAME_SHORT_LEN + 1));
+        buffer[0] = 0x00;
+        buffer[1] = (uint8_t) len_int;
+        memcpy(&buffer[2], txt, len_int);
+    }
 
     while (c) {
         dbg("write to %u\n", c->state);
-        if ((ST_RDY == c->state) && (pico_socket_write(c->sock, msg, len + 2) > 0))
+        if ((ST_RDY == c->state) && (pico_socket_write(c->sock, buffer, total_size) > 0))
             ret++;
 
         c = c->next;
     }
-    pico_free(msg);
+    pico_free(buffer);
     return ret;
 }
 
 int zmq_recv(ZMQ z, char *txt)
 {
-    int ret;
-    struct zmq_msg msg;
+    uint16_t ret;
     struct zmq_connector *nxt, *c = z->subs;
     if (z->state != ST_RDY)
+    {
+        dbg("State is not ready\n");
         return 0;
+    }
 
     while (c) {
         nxt = c->next;
-        ret = pico_socket_read(c->sock, &msg, 2);
-        if (ret < 0) {
-            dbg("Error reading!\n");
-            zmq_connector_del(c);
-        } else if (ret < 2) {
-            c->state = ST_BUSY;
-        } else {
-            return pico_socket_read(c->sock, txt, msg.len);
-        }
 
+        if(ST_RD_RDY == c->read_state)
+        {
+            ret = (uint16_t) c->msg_read.len;
+            memcpy(txt, c->msg_read.buf, ret);
+
+            /* Cleanup object */
+            free(c->msg_read.buf);
+            c->msg_read.len = 0;
+            c->bytes_received = 0;
+            c->read_state = ST_RD_FLAG;
+            return ret;
+        }
         c = nxt;
     }
     zmq_check_state(z);
