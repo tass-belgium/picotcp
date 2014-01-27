485eceb2b0d93982f5dbfd6931cc9d1b1884071e
Daniele Lacamera <daniele.lacamera@tass.be>
2014-01-27 17:12:03 +0100

    Style fixed

--- test/unit/unit_rbtree.c
+++ test/unit/unit_rbtree.c
@@ -23,20 +23,20 @@ START_TEST (test_rbtree2)
 
 #if 0
     srand48(RBTEST_SIZE); /* use test-size as salt */
-    for (i = 0; i < RBTEST_SIZE >> 1; i++)
+    for (i = 0; i < RBTEST_SIZE > > 1; i++)
     {
-      e = malloc(sizeof(elem));
-      e->value = lrand48() % RBTEST_SIZE;
-      if (pico_tree_findKey(&test_tree2, e)) {
-          free(e);
-      } else {
-          pico_tree_insert(&test_tree2, e);
-      }
+        e = malloc(sizeof(elem));
+        e->value = lrand48() % RBTEST_SIZE;
+        if (pico_tree_findKey(&test_tree2, e)) {
+            free(e);
+        } else {
+            pico_tree_insert(&test_tree2, e);
+        }
     }
     gettimeofday(&end, 0);
     printf("Rbtree test 2 inserted %d entries in %d milliseconds\n", RBTEST_SIZE,
            (int)((end.tv_sec - start.tv_sec) * 1000 + (end.tv_usec - start.tv_usec) / 1000));
-    last = 0; 
+    last = 0;
     pico_tree_foreach(s, &test_tree2){
         fail_if (last > ((elem *)(s->keyValue))->value, "error");
         last = ((elem *)(s->keyValue))->value;
@@ -78,7 +78,7 @@ START_TEST (test_rbtree)
     fail_if(!e, "Search failed...");
     fail_if(e->value != t.value, "Wrong element returned...");
 
-//    pico_tree_foreach_reverse_safe(s, &test_tree, tmp){
+/*    pico_tree_foreach_reverse_safe(s, &test_tree, tmp){ */
     pico_tree_foreach_reverse(s, &test_tree){
         fail_if(!s, "Reverse safe returned null");
         e = (elem *)pico_tree_delete(&test_tree, s->keyValue);
