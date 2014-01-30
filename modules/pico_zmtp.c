f7c902cc541eaf597663a75ec2ca812f54513a40
Mathias Devos <mathias.devos@tass.be>
2014-01-30 13:24:54 +0100

    Added function headers to zmtp.c file

--- modules/pico_zmtp.c
+++ modules/pico_zmtp.c
@@ -12,17 +12,54 @@
 
 
 struct zmtp_socket {
-    struct pico_socket *sock;
+    struct pico_socket* sock;
     enum zmq_state state;
-    zmq_socket parent;
+    zmq_socket* parent;
     enum zmq_socket_type role;
     uint16_t bytes_received;
 };
 
+static void zmtp_tcp_cb(uint16_t ev, struct pico_socket *s)
+{
+    return;
+}
+
+int8_t zmtp_bind(struct zmtp_socket* s, void* local_addr, uint16_t* port)
+{
+    return 0;
+}
+
+int8_t zmtp_connect(struct zmtp_socket* s, void* srv_addr, uint16_t remote_port);
+{
+    return 0;
+}
+
+int8_t zmtp_send(struct zmtp_socket* s, struct zmq_msg** msg, uint16_t len);
+{
+    return 0;
+}
+
+int8_t zmtp_socket_close(struct zmtp_socket *s);
+{
+    return 0;
+}
+
+int8_t zmtp_read(struct smtp_socket* s, void* buf, uint16_t len);
+{
+    return 0;
+}
+
+
 struct zmtp_socket* zmtp_socket_open(uint16_t net, uint16_t proto, struct zmq_socket* parent, enum zmq_socket_t type, void (*wakeup)(uint16_t ev, struct zmtp_socket* s));
 {  
     struct zmtp_socket* s;
-    s.parent = parent;
+//    s = pico_zalloc(sizeof(zmtp_socket));
+//    if (s == NULL)
+//       return s;
     
-    struct* pico_socket = pico_socket_open(net, proto, 
-
+//    s->parent = parent;
+    
+//    struct* pico_socket = pico_socket_open(net, proto, &zmtp_tcp_cb);
+//    s->
+    return s;
+}
