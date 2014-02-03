f3b62a9abec936716885b0aaf83261c2eeda5b57
Sam Van Den Berge <sam.van.den.berge@tass.be>
2014-02-03 12:16:05 +0100

    Added missing include

--- modules/pico_zmtp.h
+++ modules/pico_zmtp.h
@@ -9,6 +9,7 @@
 #define __PICO_ZMTP_H
 
 #include <stdint.h>
+#include "pico_socket.h"
 
 enum zmq_socket_t {
     TYPE_PUBLISHER, 
@@ -42,7 +43,6 @@ struct zmq_msg {
 };
 
 struct zmtp_socket* zmtp_socket_open(uint16_t net, uint16_t proto, enum zmq_socket_t type, void (*wakeup)(uint16_t ev, struct zmtp_socket* s));
-static void zmtp_tcp_cb(uint16_t ev, struct pico_socket* s);
 int8_t zmtp_socket_bind(struct zmtp_socket* s, void* local_addr, uint16_t* port);
 int8_t zmtp_socket_connect(struct zmtp_socket* s, void* srv_addr, uint16_t remote_port);
 int8_t zmtp_socket_send(struct zmtp_socket* s, struct zmq_msg** msg, uint16_t len);
