a5bb33d3aaee7246f063585e27a96a348fafaba9
Maxime Vincent <maxime.vincent@tass.be>
2014-02-06 12:59:46 +0100

    llu typo 2

--- stack/pico_stack.c
+++ stack/pico_stack.c
@@ -832,7 +832,7 @@ struct pico_timer *pico_timer_add(pico_time expire, void (*timer)(pico_time, voi
                 trf = &Timers->top[timer_it];
                 tmr = trf->tmr;
                 //if (tmr && tmr->caller)
-                jenkins_dbg("timer %d [%p] - caller: %p - exp:%d\n",Timers->n - timer_it , tmr, trf->caller,(uint32_t)(trf->expire - PICO_TIME_MS()));    
+                jenkins_dbg("timer %d [%p] - caller: %p - exp:%lu\n",Timers->n - timer_it , tmr, trf->caller,(uint32_t)(trf->expire - PICO_TIME_MS()));    
             }
         }
         #endif
