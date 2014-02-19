ff65cdd0ddd2ad5c22b7da8be6ce6b5c21dd13c8
Daniele Lacamera <daniele.lacamera@tass.be>
2014-02-19 10:40:32 +0100

    Test: refactoring of udp delivery function

--- modules/pico_socket_udp.c
+++ modules/pico_socket_udp.c
@@ -24,55 +24,61 @@ struct pico_socket *pico_socket_udp_open(void)
 }
 
 
+static int pico_socket_udp_deliver_ipv4(struct pico_socket *s, struct pico_frame *f)
+{
+    struct pico_ip4 s_local, p_dst;
+    struct pico_ipv4_hdr *ip4hdr;
+    struct pico_frame *cpy;
+    ip4hdr = (struct pico_ipv4_hdr*)(f->net_hdr);
+    s_local.addr = s->local_addr.ip4.addr;
+    p_dst.addr = ip4hdr->dst.addr;
+    if ((pico_ipv4_is_broadcast(p_dst.addr)) || pico_ipv4_is_multicast(p_dst.addr)) {
+        struct pico_device *dev = pico_ipv4_link_find(&s->local_addr.ip4);
+        if (pico_ipv4_is_multicast(p_dst.addr) && (pico_socket_mcast_filter(s, &ip4hdr->dst, &ip4hdr->src) < 0))
+            return -1;
+
+        if ((s_local.addr == PICO_IPV4_INADDR_ANY) || /* If our local ip is ANY, or.. */
+            (dev == f->dev)) { /* the source of the bcast packet is a neighbor... */
+            cpy = pico_frame_copy(f);
+            if (!cpy)
+                return -1;
+
+            if (pico_enqueue(&s->q_in, cpy) > 0) {
+                if (s->wakeup)
+                    s->wakeup(PICO_SOCK_EV_RD, s);
+            }
+            else
+                pico_frame_discard(cpy);
+
+        }
+    } else if ((s_local.addr == PICO_IPV4_INADDR_ANY) || (s_local.addr == p_dst.addr))
+    { /* Either local socket is ANY, or matches dst */
+        cpy = pico_frame_copy(f);
+        if (!cpy)
+            return -1;
+
+        if (pico_enqueue(&s->q_in, cpy) > 0) {
+            if (s->wakeup)
+                s->wakeup(PICO_SOCK_EV_RD, s);
+        }
+    }
+    pico_frame_discard(f);
+    return 0;
+}
+
+
 int pico_socket_udp_deliver(struct pico_sockport *sp, struct pico_frame *f)
 {
     struct pico_tree_node *index = NULL;
     struct pico_tree_node *_tmp;
     struct pico_socket *s = NULL;
-    struct pico_frame *cpy;
     pico_err = PICO_ERR_EPROTONOSUPPORT;
     #ifdef PICO_SUPPORT_UDP
     pico_err = PICO_ERR_NOERR;
     pico_tree_foreach_safe(index, &sp->socks, _tmp){
         s = index->keyValue;
         if (IS_IPV4(f)) { /* IPV4 */
-            struct pico_ip4 s_local, p_dst;
-            struct pico_ipv4_hdr *ip4hdr;
-            ip4hdr = (struct pico_ipv4_hdr*)(f->net_hdr);
-            s_local.addr = s->local_addr.ip4.addr;
-            p_dst.addr = ip4hdr->dst.addr;
-            if ((pico_ipv4_is_broadcast(p_dst.addr)) || pico_ipv4_is_multicast(p_dst.addr)) {
-                struct pico_device *dev = pico_ipv4_link_find(&s->local_addr.ip4);
-                if (pico_ipv4_is_multicast(p_dst.addr) && (pico_socket_mcast_filter(s, &ip4hdr->dst, &ip4hdr->src) < 0))
-                    return -1;
-
-                if ((s_local.addr == PICO_IPV4_INADDR_ANY) || /* If our local ip is ANY, or.. */
-                    (dev == f->dev)) { /* the source of the bcast packet is a neighbor... */
-                    cpy = pico_frame_copy(f);
-                    if (!cpy)
-                        return -1;
-
-                    if (pico_enqueue(&s->q_in, cpy) > 0) {
-                        if (s->wakeup)
-                            s->wakeup(PICO_SOCK_EV_RD, s);
-                    }
-                    else
-                        pico_frame_discard(cpy);
-
-                }
-            } else if ((s_local.addr == PICO_IPV4_INADDR_ANY) || (s_local.addr == p_dst.addr))
-            { /* Either local socket is ANY, or matches dst */
-                cpy = pico_frame_copy(f);
-                if (!cpy)
-                    return -1;
-
-                if (pico_enqueue(&s->q_in, cpy) > 0) {
-                    if (s->wakeup)
-                        s->wakeup(PICO_SOCK_EV_RD, s);
-                }
-                else
-                    pico_frame_discard(cpy);
-            }
+            return pico_socket_udp_deliver_ipv4(s, f);
         } else {
             /*... IPv6 */
         }
@@ -80,7 +86,6 @@ int pico_socket_udp_deliver(struct pico_sockport *sp, struct pico_frame *f)
     pico_frame_discard(f);
     if (s)
         return 0;
-
     pico_err = PICO_ERR_ENXIO;
   #endif
     return -1;
