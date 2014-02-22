tass-belgium/picotcp

commit c167dc57c1c9759e02ad998d3abf2561d5de7e58

Fixed CRC regression

modules/pico_tcp.c
@@ -452,12 +452,17 @@ uint16_t pico_tcp_checksum(struct pico_frame *f)
     if (IS_IPV4(f))
         return pico_tcp_checksum_ipv4(f);
 
+    if (f->sock->net == &pico_proto_ipv4)
+        return pico_tcp_checksum_ipv4(f);
+
     #endif
     #ifdef PICO_SUPPORT_IPV6
     if (IS_IPV6(f))
         return pico_tcp_checksum_ipv6(f);
-    #endif
 
+    if (f->sock->net == &pico_proto_ipv6)
+        return pico_tcp_checksum_ipv6(f);
+    #endif
     return 0xffff;
 }
 

stack/pico_socket.c
@@ -1664,7 +1664,7 @@ static inline int pico_transport_crc_check(struct pico_frame *f)
     {
     case PICO_PROTO_TCP:
         checksum_invalid = short_be(pico_tcp_checksum(f));
-         dbg("TCP CRC validation == %u\n", checksum_invalid);
+        //dbg("TCP CRC validation == %u\n", checksum_invalid);
         if (checksum_invalid) {
             dbg("TCP CRC: validation failed!\n");
             pico_frame_discard(f);

test/picoapp.c
@@ -1458,8 +1458,10 @@ void cb_tcpbench(uint16_t ev, struct pico_socket *s)
             } else if (tcpbench_mode == TCP_BENCH_RX) {
                 /* sock_a = pico_socket_accept(s, &orig, &port); */
                 pico_socket_accept(s, &orig, &port);
+#ifdef PICO_SUPPORT_IPV6
                 pico_ipv6_to_string(peer, orig.addr);
                 printf("tcpbench> Connection established with [%s]:%d.\n", peer, short_be(port));
+#endif
             }
         }
     }
@@ -1607,9 +1609,10 @@ void app_tcpbench(char *arg)
                local_port = short_be(6666);
                pico_string_to_ipv4("10.40.0.11", &local_addr.addr);
                pico_socket_bind(s, &local_addr, &local_port);*/
-
+#ifdef PICO_SUPPORT_IPV6
             pico_string_to_ipv6(dest, server_addr.addr);
             pico_socket_connect(s, &server_addr, port_be);
+#endif
 
         }
 

commit 4a284a4c878e2574bac145cf8166df9a81f4c951

Fix #115 - TCP IPv6 regression introduced in c167dc57c1c9759e02ad998d3abf2561d5de7e58

modules/pico_tcp.c
@@ -451,8 +451,7 @@ uint16_t pico_tcp_checksum(struct pico_frame *f)
     #ifdef PICO_SUPPORT_IPV4
     if (IS_IPV4(f))
         return pico_tcp_checksum_ipv4(f);
-
-    if (f->sock->net == &pico_proto_ipv4)
+    if (f->sock && (f->sock->net == &pico_proto_ipv4))
         return pico_tcp_checksum_ipv4(f);
 
     #endif
@@ -460,7 +459,7 @@ uint16_t pico_tcp_checksum(struct pico_frame *f)
     if (IS_IPV6(f))
         return pico_tcp_checksum_ipv6(f);
 
-    if (f->sock->net == &pico_proto_ipv6)
+    if (f->sock && (f->sock->net == &pico_proto_ipv6))
         return pico_tcp_checksum_ipv6(f);
     #endif
     return 0xffff;

test/autotest.sh
@@ -18,12 +18,11 @@ echo "PING6 TEST"
 killall picoapp6.elf
 
 
-# WIP
-#echo "TCP6 TEST"
-#(./build/test/picoapp6.elf --vde pic0,/tmp/pic0.ctl,aaaa::1,ffff::, -a tcpbench,r,6667,) &
-#time (./build/test/picoapp6.elf --vde pic0,/tmp/pic0.ctl,aaaa::2,ffff::, -a tcpbench,t,aaaa::1,6667, || exit 1)
-#killall picoapp6.elf
-#
+echo "TCP6 TEST"
+(./build/test/picoapp6.elf --vde pic0,/tmp/pic0.ctl,aaaa::1,ffff::, -a tcpbench,r,6667,) &
+time (./build/test/picoapp6.elf --vde pic0,/tmp/pic0.ctl,aaaa::2,ffff::, -a tcpbench,t,aaaa::1,6667, || exit 1)
+killall picoapp6.elf
+
 echo "UDP6 TEST"
 (./build/test/picoapp6.elf --vde pic0,/tmp/pic0.ctl,aaaa::1,ffff::, -a udpecho,aaaa::1,6667,) &
 ./build/test/picoapp6.elf --vde pic0,/tmp/pic0.ctl,aaaa::2,ffff::, -a udpclient,aaaa::1,6667,6667,1400,100,10, || exit 1