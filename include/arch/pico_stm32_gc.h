6b6d76cb825c17b238e3665442cde3fc1dba39c4
Maxime Vincent <maxime.vincent@tass.be>
2014-03-06 15:52:34 +0100

    add lpc43xx arch

--- include/arch/pico_stm32_gc.h
+++ include/arch/pico_stm32_gc.h
@@ -50,34 +50,6 @@ static inline void PICO_IDLE(void)
 }
 
 #else /* NO RTOS SUPPORT */
-    #define pico_free(x) free(x)
-
-static inline void *pico_zalloc(size_t size)
-{
-    void *ptr = malloc(size);
-
-    if(ptr)
-        memset(ptr, 0u, size);
-
-    return ptr;
-}
-
-static inline unsigned long PICO_TIME(void)
-{
-    register uint32_t tick = __stm32_tick;
-    return tick / 1000;
-}
-
-static inline unsigned long PICO_TIME_MS(void)
-{
-    return __stm32_tick;
-}
-
-static inline void PICO_IDLE(void)
-{
-    uint32_t now = PICO_TIME_MS();
-    while(now == PICO_TIME_MS()) ;
-}
-
+    #error Not implemented for STM32_GC
 #endif /* IFNDEF RTOS */
 
--- test/examples/test_wget_forever_tun.sh
+++ test/examples/test_wget_forever_tun.sh
@@ -10,5 +10,6 @@ sudo iptables -A FORWARD -i wlan0 -o tun0 -j ACCEPT
 $(sleep 3 && sudo ifconfig tun0 10.70.0.1/24 up) &
 #sudo ./build/test/picoapp.elf --tun tun0:10.70.0.10:255.255.255.0:10.70.0.1 -a wget_forever:10.70.0.1/STM32F4xx_Clock_Configuration_V1.1.0_configured.xls:10.70.0.1/zMidi_synth-debug-unaligned.apk:10.70.0.1/module3/libopencm3.tar.bz2
 sudo ./build/test/picoapp.elf --tun tun0:10.70.0.10:255.255.255.0:10.70.0.1 -a wget_forever:10.70.0.1/test1.bin:10.70.0.1/test10.bin:10.70.0.1/test5.bin:
+#sudo gdb --args ./build/test/picoapp.elf --tun tun0:10.70.0.10:255.255.255.0:10.70.0.1 -a wget_forever:10.70.0.1/STM32F4xx_Clock_Configuration_V1.1.0_configured.xls:10.70.0.1/zMidi_synth-debug-unaligned.apk:10.70.0.1/module3/libopencm3.tar.bz2
 sudo iptables-restore iptable_saved
 
