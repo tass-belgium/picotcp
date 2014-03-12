fb5fec58adc644b8e2d9b851b206a544e05f7c23
Daniele Lacamera <daniele.lacamera@tass.be>
2014-03-12 17:31:25 +0100

    Added unit test module for pico_tcp.c

--- test/units.sh
+++ test/units.sh
@@ -5,6 +5,7 @@ rm -f /tmp/pico-mem-report-*
 ./build/test/modunit_pico_protocol.elf || exit 1
 ./build/test/modunit_pico_frame.elf || exit 1
 ./build/test/modunit_seq.elf || exit 1
+./build/test/modunit_tcp.elf || exit 1
 
 MAXMEM=`cat /tmp/pico-mem-report-* | sort -r -n |head -1`
 echo
