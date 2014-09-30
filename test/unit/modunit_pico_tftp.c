4c0145e4bec85a7a41a07bcac16898bbfd756f41
Michele Di Pede <michele.di.pede@tass.be>
2014-09-30 13:03:40 +0200

    Increased code coverage

--- test/unit/modunit_pico_tftp.c
+++ test/unit/modunit_pico_tftp.c
@@ -66,6 +66,17 @@ START_TEST(tc_check_opcode)
 }
 END_TEST
 
+START_TEST(tc_find_session_by_socket)
+{
+    tftp_sessions = (struct pico_tftp_session *)PICO_ZALLOC(sizeof(struct pico_tftp_session));
+    tftp_sessions->socket = &example_socket;
+    tftp_sessions->next = (struct pico_tftp_session *)PICO_ZALLOC(sizeof(struct pico_tftp_session));
+    tftp_sessions->socket = NULL;
+    tftp_sessions->next = NULL;
+    fail_if(find_session_by_socket(&example_socket) != tftp_sessions->next);
+}
+END_TEST
+
 START_TEST(tc_tftp_finish)
 {
     tftp_sessions = 0;
@@ -208,9 +219,6 @@ END_TEST
 START_TEST(tc_pico_tftp_abort)
 {
     int ret;
-    union pico_address address;
-    struct pico_tftp_session *session = NULL;
-
     listen_socket = NULL;
 
     /*first case: no session and no listening socket*/
@@ -253,21 +261,6 @@ START_TEST(tc_tftp_data_err)
     /* TODO: test this: static void tftp_data_err(uint8_t *block, uint32_t len, union pico_address *a, uint16_t port) */
 }
 END_TEST
-START_TEST(tc_tftp_fsm_receive_request)
-{
-    /* TODO: test this: static void tftp_fsm_receive_request(uint8_t *block, uint32_t r, union pico_address *a, uint16_t port) */
-}
-END_TEST
-START_TEST(tc_tftp_fsm_receive)
-{
-    /* TODO: test this: static void tftp_fsm_receive(uint8_t *block, uint32_t r, union pico_address *a, uint16_t port) */
-}
-END_TEST
-START_TEST(tc_tftp_fsm_error)
-{
-    /* TODO: test this: static void tftp_fsm_error(uint8_t *block, uint32_t r, union pico_address *a, uint16_t port) */
-}
-END_TEST
 START_TEST(tc_tftp_fsm_timeout)
 {
     /* TODO: test this: static void tftp_fsm_timeout(pico_time now, void *arg) */
@@ -283,11 +276,6 @@ START_TEST(tc_tftp_cb)
     /* TODO: test this: static void tftp_cb(uint16_t ev, struct pico_socket *s) */
 }
 END_TEST
-START_TEST(tc_tftp_bind)
-{
-    /* TODO: test this: static void tftp_bind(void) */
-}
-END_TEST
 START_TEST(tc_tftp_socket_open)
 {
     /* TODO: test this: static int tftp_socket_open(uint16_t family, union pico_address *a, uint16_t port) */
@@ -300,6 +288,7 @@ Suite *pico_suite(void)
     Suite *s = suite_create("PicoTCP");
 
     TCase *TCase_check_opcode = tcase_create("Unit test for check_opcode");
+    TCase *TCase_find_session_by_socket = tcase_create("Unit test for find_session_by_socket");
     TCase *TCase_tftp_finish = tcase_create("Unit test for tftp_finish");
     TCase *TCase_tftp_send_ack = tcase_create("Unit test for tftp_send_ack");
     TCase *TCase_tftp_send_req = tcase_create("Unit test for tftp_send_req");
@@ -313,18 +302,16 @@ Suite *pico_suite(void)
     TCase *TCase_tftp_timeout = tcase_create("Unit test for tftp_timeout");
     TCase *TCase_tftp_req = tcase_create("Unit test for tftp_req");
     TCase *TCase_tftp_data_err = tcase_create("Unit test for tftp_data_err");
-    TCase *TCase_tftp_fsm_receive_request = tcase_create("Unit test for tftp_fsm_receive_request");
-    TCase *TCase_tftp_fsm_receive = tcase_create("Unit test for tftp_fsm_receive");
-    TCase *TCase_tftp_fsm_error = tcase_create("Unit test for tftp_fsm_error");
     TCase *TCase_tftp_fsm_timeout = tcase_create("Unit test for tftp_fsm_timeout");
     TCase *TCase_tftp_receive = tcase_create("Unit test for tftp_receive");
     TCase *TCase_tftp_cb = tcase_create("Unit test for tftp_cb");
-    TCase *TCase_tftp_bind = tcase_create("Unit test for tftp_bind");
     TCase *TCase_tftp_socket_open = tcase_create("Unit test for tftp_socket_open");
 
 
     tcase_add_test(TCase_check_opcode, tc_check_opcode);
     suite_add_tcase(s, TCase_check_opcode);
+    tcase_add_test(TCase_find_session_by_socket, tc_find_session_by_socket);
+    suite_add_tcase(s, TCase_find_session_by_socket);
     tcase_add_test(TCase_tftp_finish, tc_tftp_finish);
     suite_add_tcase(s, TCase_tftp_finish);
     tcase_add_test(TCase_tftp_send_ack, tc_tftp_send_ack);
@@ -351,20 +338,12 @@ Suite *pico_suite(void)
     suite_add_tcase(s, TCase_tftp_req);
     tcase_add_test(TCase_tftp_data_err, tc_tftp_data_err);
     suite_add_tcase(s, TCase_tftp_data_err);
-    tcase_add_test(TCase_tftp_fsm_receive_request, tc_tftp_fsm_receive_request);
-    suite_add_tcase(s, TCase_tftp_fsm_receive_request);
-    tcase_add_test(TCase_tftp_fsm_receive, tc_tftp_fsm_receive);
-    suite_add_tcase(s, TCase_tftp_fsm_receive);
-    tcase_add_test(TCase_tftp_fsm_error, tc_tftp_fsm_error);
-    suite_add_tcase(s, TCase_tftp_fsm_error);
     tcase_add_test(TCase_tftp_fsm_timeout, tc_tftp_fsm_timeout);
     suite_add_tcase(s, TCase_tftp_fsm_timeout);
     tcase_add_test(TCase_tftp_receive, tc_tftp_receive);
     suite_add_tcase(s, TCase_tftp_receive);
     tcase_add_test(TCase_tftp_cb, tc_tftp_cb);
     suite_add_tcase(s, TCase_tftp_cb);
-    tcase_add_test(TCase_tftp_bind, tc_tftp_bind);
-    suite_add_tcase(s, TCase_tftp_bind);
     tcase_add_test(TCase_tftp_socket_open, tc_tftp_socket_open);
     suite_add_tcase(s, TCase_tftp_socket_open);
     return s;
