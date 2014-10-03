a7b9ecc29dbdf3ad793169e0abc20473321ce7c4
Maxime Vincent <maxime.vincent@tass.be>
2014-10-03 11:55:02 +0200

    Add windows TAP driver; initial version.

---
+++ modules/pico_dev_tap_windows.h
@@ -0,0 +1,17 @@
+/*********************************************************************
+   PicoTCP. Copyright (c) 2014 TASS Belgium NV. Some rights reserved.
+   See LICENSE and COPYING for usage.
+
+ *********************************************************************/
+#ifndef INCLUDE_PICO_TAP
+#define INCLUDE_PICO_TAP
+#include "pico_config.h"
+#include "pico_device.h"
+
+/* will look for the first TAP device available, and use it */
+struct pico_device *pico_tap_create(char *name, uint8_t *mac);
+/* TODO: not implemented yet */
+/* void pico_tap_destroy(struct pico_device *null); */
+
+#endif
+
