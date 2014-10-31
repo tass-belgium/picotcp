00bf27fb8f834ed734d46b33527c0d78e0bc634d
Toon Stegen <toon.stegen@tass.be>
2014-10-31 18:07:42 +0100

    Revert "Check for root and remove old sockets"
    This reverts commit 7a20f9b13dca86fc3b9c36905cf3ec3fa18b9965.
    Jenkins doen't run the autotest as root, so better not check for this.

--- test/vde_sock_start_user.sh
+++ test/vde_sock_start_user.sh
@@ -1,15 +1,7 @@
 #!/bin/bash
 
-# Make sure only root can run our script
-if [[ $EUID -ne 0 ]]; then
-   echo "This script must be run as root" 1>&2
-   exit 1
-fi
-
 vdecmd -s /tmp/pico.mgmt shutdown
 vdecmd -s /tmp/pici.mgmt shutdown
-rm -rf /tmp/pic0.ctl
-rm -rf /tmp/pic1.ctl
 vde_switch -s /tmp/pic0.ctl -m 777 -M /tmp/pico.mgmt -d -hub -t vde0
 vde_switch -s /tmp/pic1.ctl -m 777 -M /tmp/pici.mgmt -d -hub
 /sbin/ifconfig vde0 10.50.0.1 netmask 255.255.0.0
