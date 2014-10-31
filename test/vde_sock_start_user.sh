186608f7478f7d48f2c775ab0a0de2d465c25605
Toon Stegen <toon.stegen@tass.be>
2014-10-31 17:02:17 +0100

    Use path of ifconfig
    Jenkins could not find ifconfig, so we supply the full path.

--- test/vde_sock_start_user.sh
+++ test/vde_sock_start_user.sh
@@ -4,4 +4,4 @@ vdecmd -s /tmp/pico.mgmt shutdown
 vdecmd -s /tmp/pici.mgmt shutdown
 vde_switch -s /tmp/pic0.ctl -m 777 -M /tmp/pico.mgmt -d -hub -t vde0
 vde_switch -s /tmp/pic1.ctl -m 777 -M /tmp/pici.mgmt -d -hub
-ifconfig vde0 10.50.0.1 netmask 255.255.0.0
+/sbin/ifconfig vde0 10.50.0.1 netmask 255.255.0.0
