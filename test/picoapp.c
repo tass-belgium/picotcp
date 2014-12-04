d6db4335e2ce414c25a4bce67c811cf3cd1fb535
Daniele Lacamera <daniele.lacamera@tass.be>
2014-12-04 17:05:11 +0100

    Added ifdefs to picoapp's olsr, so it does not try to compile when disabled

--- test/picoapp.c
+++ test/picoapp.c
@@ -585,6 +585,7 @@ int main(int argc, char **argv)
 #endif
             } else IF_APPNAME("noop") {
                 app_noop();
+#ifdef PICO_SUPPORT_OLSR
             } else IF_APPNAME("olsr") {
                 pico_olsr_init();
                 dev = pico_get_device("pic0");
@@ -598,6 +599,7 @@ int main(int argc, char **argv)
                 }
 
                 app_noop();
+#endif
             } else IF_APPNAME("slaacv4") {
 #ifndef PICO_SUPPORT_SLAACV4
                 return 0;
