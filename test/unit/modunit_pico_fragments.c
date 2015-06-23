9614a5039442fd9c9688cf2a076f875d265e2054
Daniele Lacamera <daniele.lacamera@tass.be>
2015-06-23 19:48:04 +0200

    Fixed pico_fragment.c unit tests composition (removing auto-generated duplicates)

--- test/unit/modunit_pico_fragments.c
+++ test/unit/modunit_pico_fragments.c
@@ -18,60 +18,6 @@
 
 START_TEST(tc_pico_ipv6_frag_compare)
 {
-}
-END_TEST
-START_TEST(tc_pico_ipv4_frag_compare)
-{
-   /* TODO: test this: static int pico_ipv4_frag_compare(void *ka, void *kb); */
-}
-END_TEST
-START_TEST(tc_pico_ipv6_fragments_complete)
-{
-   /* TODO: test this: static void pico_ipv6_fragments_complete(unsigned int len, uint8_t proto); */
-}
-END_TEST
-START_TEST(tc_pico_ipv4_fragments_complete)
-{
-   /* TODO: test this: static void pico_ipv4_fragments_complete(unsigned int len, uint8_t proto); */
-}
-END_TEST
-START_TEST(tc_pico_fragments_complete)
-{
-   /* TODO: test this: static void pico_fragments_complete(unsigned int bookmark, uint8_t proto, uint8_t net); */
-}
-END_TEST
-START_TEST(tc_pico_fragments_check_complete)
-{
-   /* TODO: test this: static void pico_fragments_check_complete(uint8_t proto, uint8_t net); */
-}
-END_TEST
-START_TEST(tc_pico_frag_expire)
-{
-   /* TODO: test this: static void pico_frag_expire(pico_time now, void *arg); */
-}
-END_TEST
-START_TEST(tc_pico_ipv6_frag_timer_on)
-{
-   /* TODO: test this: static void pico_ipv6_frag_timer_on(void); */
-}
-END_TEST
-START_TEST(tc_pico_ipv4_frag_timer_on)
-{
-   /* TODO: test this: static void pico_ipv4_frag_timer_on(void); */
-}
-END_TEST
-START_TEST(tc_pico_ipv6_frag_match)
-{
-   /* TODO: test this: static int pico_ipv6_frag_match(struct pico_frame *a, struct pico_frame *b); */
-}
-END_TEST
-START_TEST(tc_pico_ipv4_frag_match)
-{
-   /* TODO: test this: static int pico_ipv4_frag_match(struct pico_frame *a, struct pico_frame *b); */
-}
-END_TEST
-START_TEST(tc_pico_ipv6_frag_compare)
-{
    /* TODO: test this: static int pico_ipv6_frag_compare(void *ka, void *kb) */
 }
 END_TEST
@@ -142,41 +88,8 @@ Suite *pico_suite(void)
     TCase *TCase_pico_ipv4_frag_timer_on = tcase_create("Unit test for pico_ipv4_frag_timer_on");
     TCase *TCase_pico_ipv6_frag_match = tcase_create("Unit test for pico_ipv6_frag_match");
     TCase *TCase_pico_ipv4_frag_match = tcase_create("Unit test for pico_ipv4_frag_match");
-    TCase *TCase_pico_ipv6_frag_compare = tcase_create("Unit test for pico_ipv6_frag_compare");
-    TCase *TCase_pico_ipv4_frag_compare = tcase_create("Unit test for pico_ipv4_frag_compare");
-    TCase *TCase_pico_ipv6_fragments_complete = tcase_create("Unit test for pico_ipv6_fragments_complete");
-    TCase *TCase_pico_ipv4_fragments_complete = tcase_create("Unit test for pico_ipv4_fragments_complete");
-    TCase *TCase_pico_fragments_complete = tcase_create("Unit test for pico_fragments_complete");
-    TCase *TCase_pico_fragments_check_complete = tcase_create("Unit test for pico_fragments_check_complete");
-    TCase *TCase_pico_frag_expire = tcase_create("Unit test for pico_frag_expire");
-    TCase *TCase_pico_ipv6_frag_timer_on = tcase_create("Unit test for pico_ipv6_frag_timer_on");
-    TCase *TCase_pico_ipv4_frag_timer_on = tcase_create("Unit test for pico_ipv4_frag_timer_on");
-    TCase *TCase_pico_ipv6_frag_match = tcase_create("Unit test for pico_ipv6_frag_match");
-    TCase *TCase_pico_ipv4_frag_match = tcase_create("Unit test for pico_ipv4_frag_match");
 
 
-    tcase_add_test(TCase_pico_ipv6_frag_compare, tc_pico_ipv6_frag_compare);
-    suite_add_tcase(s, TCase_pico_ipv6_frag_compare);
-    tcase_add_test(TCase_pico_ipv4_frag_compare, tc_pico_ipv4_frag_compare);
-    suite_add_tcase(s, TCase_pico_ipv4_frag_compare);
-    tcase_add_test(TCase_pico_ipv6_fragments_complete, tc_pico_ipv6_fragments_complete);
-    suite_add_tcase(s, TCase_pico_ipv6_fragments_complete);
-    tcase_add_test(TCase_pico_ipv4_fragments_complete, tc_pico_ipv4_fragments_complete);
-    suite_add_tcase(s, TCase_pico_ipv4_fragments_complete);
-    tcase_add_test(TCase_pico_fragments_complete, tc_pico_fragments_complete);
-    suite_add_tcase(s, TCase_pico_fragments_complete);
-    tcase_add_test(TCase_pico_fragments_check_complete, tc_pico_fragments_check_complete);
-    suite_add_tcase(s, TCase_pico_fragments_check_complete);
-    tcase_add_test(TCase_pico_frag_expire, tc_pico_frag_expire);
-    suite_add_tcase(s, TCase_pico_frag_expire);
-    tcase_add_test(TCase_pico_ipv6_frag_timer_on, tc_pico_ipv6_frag_timer_on);
-    suite_add_tcase(s, TCase_pico_ipv6_frag_timer_on);
-    tcase_add_test(TCase_pico_ipv4_frag_timer_on, tc_pico_ipv4_frag_timer_on);
-    suite_add_tcase(s, TCase_pico_ipv4_frag_timer_on);
-    tcase_add_test(TCase_pico_ipv6_frag_match, tc_pico_ipv6_frag_match);
-    suite_add_tcase(s, TCase_pico_ipv6_frag_match);
-    tcase_add_test(TCase_pico_ipv4_frag_match, tc_pico_ipv4_frag_match);
-    suite_add_tcase(s, TCase_pico_ipv4_frag_match);
     tcase_add_test(TCase_pico_ipv6_frag_compare, tc_pico_ipv6_frag_compare);
     suite_add_tcase(s, TCase_pico_ipv6_frag_compare);
     tcase_add_test(TCase_pico_ipv4_frag_compare, tc_pico_ipv4_frag_compare);
