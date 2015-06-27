7556e8e9d6f2b2c4e6b29fa8f103403845f5e79b
Daniele Lacamera <daniele.lacamera@tass.be>
2015-06-27 10:55:23 +0200

    tickless: iperf test to check socket functionality

--- test/tickless.c
+++ test/tickless.c
@@ -4,8 +4,84 @@
 #include "pico_ipv4.h"
 #include "pico_ipv6.h"
 #include "pico_dev_tap.h"
+#include "pico_socket.h"
+
+#define DURATION 30
 
 
+struct iperf_hdr {
+    int32_t flags;          /* 0 */
+    int32_t numThreads;     /* 1 */
+    int32_t mPort;          /* 5001  */
+    int32_t bufferlen;      /* 0 */ 
+    int32_t mWinBand;       /* 0 */
+    int32_t mAmount;        /* 0xfffffc18 */
+};
+
+#define IPERF_PORT 5001
+#define MTU 1444
+static pico_time deadline;
+
+static void panic(void)
+{
+    for(;;);
+}
+
+static char buf[MTU] = {};
+
+static void buf_paint(void)
+{
+    char paint[11] = "0123456789";
+    int i;
+    for (i = 0; i < MTU; i++) {
+        buf[i] = paint[i % 10];
+    }
+}
+
+static void send_hdr(struct pico_socket *s)
+{
+    struct iperf_hdr hdr = {} ;
+    hdr.numThreads = long_be(1);
+    hdr.mPort = long_be(5001);
+    hdr.mAmount = long_be(0xfffffc18);
+    pico_socket_write(s, &hdr, sizeof(hdr));
+    deadline = PICO_TIME_MS() + DURATION * 1000;
+}
+
+static void iperf_cb(uint16_t ev, struct pico_socket *s)
+{
+    int r;
+    if (ev & PICO_SOCK_EV_CONN) {
+        printf("Connected!\n");
+        send_hdr(s);
+        return;
+    }
+
+    if (ev & PICO_SOCK_EV_WR) {
+        if (PICO_TIME_MS() > deadline) {
+            pico_socket_close(s);
+            return;
+        }
+        pico_socket_write(s, buf, MTU);
+    }
+}
+
+
+static void socket_setup(void)
+{
+	int yes = 1;
+    uint16_t send_port = 0;
+    struct pico_socket *s = NULL;
+    union pico_address dst = {
+        .ip4 = {0}, .ip6 = {{0}}
+    };
+
+    pico_string_to_ipv4("192.168.2.1", &dst.ip4.addr);
+    send_port = short_be(5001);
+    s = pico_socket_open(PICO_PROTO_IPV4, PICO_PROTO_TCP, &iperf_cb);
+    pico_socket_connect(s, &dst.ip4, send_port);
+    return;
+}
 
 int main(void)
 {
@@ -15,6 +91,7 @@ int main(void)
     struct pico_device *tap;
 
     uint8_t mac[6] = {0x00,0x00,0x00,0x12,0x34,0x56};
+    struct pico_socket *s;
 
     pico_stack_init();
 
@@ -34,10 +111,12 @@ int main(void)
     }
 #endif
 
+    socket_setup();
+
     while(1) {
         interval = pico_stack_go();
         if (interval != 0) {
-            printf("Interval: %lld\n", interval);
+//            printf("Interval: %lld\n", interval);
             pico_tap_WFI(tap, interval);
         }
     }
