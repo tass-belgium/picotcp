9f76f3793db002339fa2cb0eb18f211383de2c40
roel0 <roel.postelmans@altran.com>
2015-11-21 21:44:50 +0000

    Protected igmp api calls with ifdef

--- modules/pico_ipv4.c
+++ modules/pico_ipv4.c
@@ -740,8 +740,9 @@ int pico_ipv4_mcast_join(struct pico_ip4 *mcast_link, struct pico_ip4 *mcast_gro
     if (g) {
         if (reference_count)
             g->reference_count++;
-
+#ifdef PICO_SUPPORT_IGMP
         pico_igmp_state_change(mcast_link, mcast_group, filter_mode, MCASTFilter, PICO_IGMP_STATE_UPDATE);
+#endif
     } else {
         g = PICO_ZALLOC(sizeof(struct pico_mcast_group));
         if (!g) {
@@ -756,7 +757,9 @@ int pico_ipv4_mcast_join(struct pico_ip4 *mcast_link, struct pico_ip4 *mcast_gro
         g->MCASTSources.root = &LEAF;
         g->MCASTSources.compare = ipv4_mcast_sources_cmp;
         pico_tree_insert(link->MCASTGroups, g);
+#ifdef PICO_SUPPORT_IGMP
         pico_igmp_state_change(mcast_link, mcast_group, filter_mode, MCASTFilter, PICO_IGMP_STATE_CREATE);
+#endif
     }
 
     if (mcast_group_update(g, MCASTFilter, filter_mode) < 0) {
@@ -794,7 +797,9 @@ int pico_ipv4_mcast_leave(struct pico_ip4 *mcast_link, struct pico_ip4 *mcast_gr
         return -1;
     } else {
         if (reference_count && (--(g->reference_count) < 1)) {
+#ifdef PICO_SUPPORT_IGMP
             pico_igmp_state_change(mcast_link, mcast_group, filter_mode, MCASTFilter, PICO_IGMP_STATE_DELETE);
+#endif
             /* cleanup filter */
             pico_tree_foreach_safe(index, &g->MCASTSources, _tmp) {
                 source = index->keyValue;
@@ -804,7 +809,9 @@ int pico_ipv4_mcast_leave(struct pico_ip4 *mcast_link, struct pico_ip4 *mcast_gr
             pico_tree_delete(link->MCASTGroups, g);
             PICO_FREE(g);
         } else {
+#ifdef PICO_SUPPORT_IGMP
             pico_igmp_state_change(mcast_link, mcast_group, filter_mode, MCASTFilter, PICO_IGMP_STATE_UPDATE);
+#endif          
             if (mcast_group_update(g, MCASTFilter, filter_mode) < 0)
                 return -1;
         }
