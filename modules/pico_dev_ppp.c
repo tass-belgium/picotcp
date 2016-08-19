a28903aef7299c9f3136775461271ced26b6a45e
Frederik Van Slycken <frederik.van.slycken@gmail.com>
2016-08-19 09:27:22 +0200

    dev_ppp currently depends on ipv4, not compatible with ipv6

--- modules/pico_dev_ppp.c
+++ modules/pico_dev_ppp.c
@@ -2224,12 +2224,7 @@ int pico_ppp_disconnect(struct pico_device *dev)
     ppp->autoreconnect = 0;
     evaluate_lcp_state(ppp, PPP_LCP_EVENT_CLOSE);
 
-#ifdef PICO_SUPPORT_IPV4
     pico_ipv4_cleanup_links(dev);
-#endif
-#ifdef PICO_SUPPORT_IPV6
-    pico_ipv6_cleanup_links(dev);
-#endif
 
     return 0;
 }
