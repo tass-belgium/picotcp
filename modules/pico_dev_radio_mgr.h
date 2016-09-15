b12deae00318eb8e4f058dd521428275eb0e1f10
jelledevleeschouwer <jelle.devleeschouwer.1@gmail.com>
2016-09-16 00:30:23 +0200

    [RADIO_MGR] - First version of sudo-less radiotest

---
+++ modules/pico_dev_radio_mgr.h
@@ -0,0 +1,14 @@
+/*********************************************************************
+   PicoTCP. Copyright (c) 2012-2015 Altran Intelligent Systems. Some rights reserved.
+   See LICENSE and COPYING for usage.
+
+   Authors: Jelle De Vleeschouwer
+ *********************************************************************/
+
+#ifndef __PICO_DEV_RADIO_MGR_H_
+#define __PICO_DEV_RADIO_MGR_H_
+
+/* Start listening for TCP connection requests on 'LISTENING_PORT' */
+int pico_radio_mgr_start(void);
+
+#endif
